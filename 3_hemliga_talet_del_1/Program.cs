ea8abd51b3b98fbe2e6ae54f6407970dc3607dcf
Frans Engström <frans.engstrom@learnet.se>
2021-01-19 12:10:57 +0100

    first commit

---
+++ 3_hemliga_talet_del_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _hemliga_talet_del_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Random rndNr = new Random(); // Startar slumpgeneratorn för nummer. 
+
+            int randomNummer =  rndNr.Next(1, 101); //Skapar ett nummer från och med 1, då minvärdet är inkluderande, upp till 100 och 101 då maxvärdet är uteslutande. 
+
+            Console.WriteLine(randomNummer); // Skriver ut numret på konsolen.
+
+            Console.ReadKey(); // Väntar på knapptryckning så att numret är läsbart innan konsolen går ner.
+        }
+    }
+}
